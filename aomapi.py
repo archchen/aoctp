5a892a725bec9874539918b88640b31d24c8e9f9
arch <chen.zhiqi@gmail.com>
2016-12-08 22:30:20 +0800

    add admapi definition

---
+++ aomapi.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from ctp.futures import ApiStruct,MdApi
+
+class AoMdApi(MdApi):
+
+    """Docstring for AoMdApi. """
+
+    def __init__(self):
+        """TODO: to be defined1. """
+        
